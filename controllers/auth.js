53dcb9f8dde43ae843ae4b9e8e07b45e68e32889
Salnikova-Anna <anna_val@ukr.net>
2023-12-04 16:23:00 +0200

    hash password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,18 @@
 const { HttpError, ctrlWrapper } = require("../helpers");
 const { User } = require("../models/user");
+const bcrypt = require("bcrypt");
 
 const register = async (req, res) => {
-  const { email } = req.body;
+  const { email, password } = req.body;
   const user = await User.findOne({ email });
 
   if (user) {
     throw HttpError(409, "Email in use");
   }
 
-  const newUser = await User.create(req.body);
+  const hashPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await User.create({ ...req.body, password: hashPassword });
 
   res.status(201).json({
     user: {
